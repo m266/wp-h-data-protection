82bbe5a5bd07aab1388f0f9718747c3e63e7b77c
m266 <herbrand@mailbox.org>
2023-02-23 09:49:44 +0100

    Update 2.3.1

--- inc/wphdp_privacy_checkbox.php
+++ inc/wphdp_privacy_checkbox.php
@@ -1,20 +1,20 @@
 <?php
 // Quelle: https://wordpress.stackexchange.com/questions/127027/how-to-add-a-privacy-checkbox-in-comment-template
- if ( is_admin() ) {  // Privacy-Checkbox beim Admin nicht anzeigen
-     }
-     else {   // Privacy-Checkbox im Frontend anzeigen
-// Quelle: https://wordpress.stackexchange.com/questions/127027/how-to-add-a-privacy-checkbox-in-comment-template
-//add your checkbox after the comment field
-add_filter('comment_form_field_comment', 'wphdp_comment_form_field_comment');
-function wphdp_comment_form_field_comment($comment_field) {
-    return $comment_field . '<input type="checkbox" name="privacy" value="privacy-key" class="privacyBox" aria-req="true"><p class="pprivacy">Ich stimme der Speicherung und Verarbeitung meiner Daten nach der DSGVO zu und akzeptiere die <a href="' . get_privacy_policy_url() . '">Datenschutzbedingungen.</a> *<p>';
-}
+if (is_admin()) { // Privacy-Checkbox beim Admin nicht anzeigen
+} else { // Privacy-Checkbox im Frontend anzeigen
+    //add your checkbox after the comment field
+    add_filter('comment_form_field_comment', 'wphdp_comment_form_field_comment');
+    function wphdp_comment_form_field_comment($comment_field)
+    {
+        return $comment_field . '<input type="checkbox" name="privacy" value="privacy-key" class="privacyBox" aria-req="true"><p class="pprivacy">Ich stimme der Speicherung und Verarbeitung meiner Daten nach der DSGVO zu und akzeptiere die ' . ((!empty($url = get_privacy_policy_url())) ? '<a class="privacy-policy-link" href="' . esc_url($url) . '">Datenschutzbedingungen</a>' : 'Datenschutzbedingungen') . '. *</p>';
+    }
 //javascript validation
-add_action('wp_footer', 'valdate_privacy_comment_javascript');
-function valdate_privacy_comment_javascript() {
-    if (is_single() && comments_open()) {
-        wp_enqueue_script('jquery');
-        ?>
+    add_action('wp_footer', 'valdate_privacy_comment_javascript');
+    function valdate_privacy_comment_javascript()
+    {
+        if (is_single() && comments_open()) {
+            wp_enqueue_script('jquery');
+            ?>
         <script type="text/javascript">
         jQuery(document).ready(function($){
             $("#submit").click(function(e){
@@ -28,15 +28,16 @@ function valdate_privacy_comment_javascript() {
         </script>
         <?php
 }
-}
-//no js fallback validation
-add_filter('preprocess_comment', 'verify_comment_privacy');
-function verify_comment_privacy($commentdata) {
-    if (!isset($_POST['privacy'])) {
-        wp_die(__('<b>Fehler:</b> Bitte zum Akzeptieren der Datenverarbeitung die Checkbox anklicken.'));
     }
+//no js fallback validation
+    add_filter('preprocess_comment', 'verify_comment_privacy');
+    function verify_comment_privacy($commentdata)
+    {
+        if (!isset($_POST['privacy'])) {
+            wp_die(__('<b>Fehler:</b> Bitte zum Akzeptieren der Datenverarbeitung die Checkbox anklicken.'));
+        }
 
-    return $commentdata;
-}
+        return $commentdata;
+    }
 }
 ?>
--- wphdp.php
+++ wphdp.php
@@ -5,8 +5,8 @@ Plugin URI:        https://github.com/m266/wp-h-data-protection
 Description:       Datenschutz f&uuml;r WordPress
 Author:            Hans M. Herbrand
 Author URI:        https://herbrand.org
-Version:           2.3
-Date:              2023-02-19
+Version:           2.3.1
+Date:              2023-02-21
 License:           GNU General Public License v2 or later
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
 GitHub Plugin URI: https://github.com/m266/wp-h-data-protection
